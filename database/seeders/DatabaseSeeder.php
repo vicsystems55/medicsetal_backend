7bccce00399ee56384ff56eb420079551b052eba
vicSystems <victorasuquob@gmail.com>
2021-12-21 14:57:31 +0100

    orders table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
         UserSeeder::class,
-        PhoenixgnPackagesSeeder::class,
-        FAQSeeder::class,
+        // PhoenixgnPackagesSeeder::class,
+        // FAQSeeder::class,
         ]);
     }
 }
